83c1cee62c0033484f2601cfe23c2d791d5047d4
foto888999 <84739938+foto888999@users.noreply.github.com>
2022-03-22 13:31:57 +0200

    deploy1

---
+++ src/App.js
@@ -0,0 +1,54 @@
+// npm start
+import React, { Component } from "react";
+
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/testAPI")
+    // fetch("http://localhost:9000/reset-password")
+        .then(res => res.text())
+        .then(res => {
+            this.setState({apiResponse: res + " moment 2 "+Date.now()},
+                (e) => {
+                    console.log("=== callAPI")
+                    console.log(this.state.apiResponse)
+                }
+            )
+        })
+        .catch(err => err);
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+
+      const onClick_sendAPI = (e) => {
+          console.log("=== onClick_sendAPI(e)")
+
+          this.callAPI();
+      }
+
+    return (
+        <div className="App">
+            <div
+                style={{width:'130px', height:'50px',
+                    display:'flex',
+                    flexDirection:'row',
+                    justifyContent:'center',
+                    backgroundColor:'red'}}>
+                <button  onClick={(e)=>onClick_sendAPI(e)}>SEND API</button>
+            </div>
+        </div>
+    );
+  }
+}
+
+export default App;
